1806cf53daa038b4d1fcc6490ee2b6e13447dfa1
Ryan Faircloth <ryan@dss-i.com>
2023-08-25 14:33:21 -0400

    feat!: initial working build

--- segateway_source_mimecast/ConfGenSource.py
+++ segateway_source_mimecast/ConfGenSource.py
@@ -1,23 +1,21 @@
-import logging
-
 try:
     from syslogng import Logger
 
     logger = Logger(__name__)
-except:
-    from logging import getLogger
+except ImportError:
+    from logging import StreamHandler, getLogger
 
     logger = getLogger(__name__)
     from pythonjsonlogger import jsonlogger
 
-    logHandler = logging.StreamHandler()
+    logHandler = StreamHandler()
     formatter = jsonlogger.JsonFormatter()
     logHandler.setFormatter(formatter)
     logger.addHandler(logHandler)
 
 try:
     from syslogng import register_config_generator
-except:
+except ImportError:
     pass
 
 from importlib import resources as impresources
@@ -26,7 +24,7 @@ from segateway_source_mimecast import conf
 
 
 def _plugin_config_generator(args):
-    inp_file = (impresources.files(conf) / 'source.conf')
+    inp_file = impresources.files(conf) / "source.conf"
     logger.error(f"Source {inp_file}")
     with inp_file.open("rt") as f:
         return f.read() + "\n"
@@ -34,7 +32,9 @@ def _plugin_config_generator(args):
 
 def register_plugin_config_generator():
     register_config_generator(
-        context="root", name="segateway_source_mimecast_source", config_generator=_plugin_config_generator
+        context="root",
+        name="segateway_source_mimecast_source",
+        config_generator=_plugin_config_generator,
     )
 
 
--- segateway_source_mimecast/LogSource.py
+++ segateway_source_mimecast/LogSource.py
@@ -1,80 +1,232 @@
-from syslogng import LogSource
-from syslogng import LogMessage
-
+import asyncio
+import gzip
+import logging
 import os
+import tempfile
+import time
+from datetime import datetime, timedelta
+from json import loads
+
+import aiofiles
+import backoff
+import httpx
+import orjson
+import pytz
 from dotenv import load_dotenv
+from furl import furl
+
+from segateway_source_mimecast._CleanEvent import CleanEvent
+
+try:
+    from syslogng import Logger
+
+    logger = Logger(__name__)
+except ImportError:
+    from pythonjsonlogger import jsonlogger
+
+    class CustomJsonFormatter(jsonlogger.JsonFormatter):
+        def add_fields(self, log_record, record, message_dict):
+            super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+            if not log_record.get("timestamp"):
+                # this doesn't use record.created, so it is slightly off
+                now = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+                log_record["timestamp"] = now
+            if log_record.get("level"):
+                log_record["level"] = log_record["level"].upper()
+            else:
+                log_record["level"] = record.levelname
+
+            log_record["threadName"] = record.threadName
+            log_record["lineno"] = record.lineno
+            log_record["module"] = record.module
+            log_record["exc_info"] = record.exc_info
+            log_record["exc_text"] = record.exc_text
+
+    # logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    logHandler = logging.StreamHandler()
+    formatter = CustomJsonFormatter()
+    logHandler.setFormatter(formatter)
+
+    logger.addHandler(logHandler)
 
 load_dotenv()
 
-from json import loads
-import gzip
-import tempfile
-import asyncio
-import backoff
-import aiofiles
-import httpx
-from datetime import timedelta, date, datetime
-import furl
+try:
+    from syslogng import LogMessage, LogSource, Persist
+
+    syslogng = True
+except ImportError:
+    syslogng = False
+
+    class LogSource:
+        pass
+
+    class LogMessage:
+        pass
 
 
-class LogSource(LogSource):
+def _backoff_handler(details):
+    logger.warning(
+        "Backing off {wait:0.1f} seconds after {tries} tries "
+        "calling function {target} with args {args} and kwargs "
+        "{kwargs}".format(**details)
+    )
+
+
+class LogSourcePlugin(LogSource):
     """Provides a syslog-ng async source for Mimecast"""
 
-    cancelled: bool = False
+    _cancelled: bool = False
 
     def init(self, options):
+        """Syslog NG doesn't use the python init so any one time setup is done here"""
+
         self._CLIENT_ID = os.environ["MIMECAST_CLIENT_ID"]
         self._CLIENT_SECRET = os.environ["MIMECAST_CLIENT_SECRET"]
         self._HOST = os.environ.get("MIMECAST_HOST", "api.services.mimecast.com")
+        self._TYPE = os.environ["MIMECAST_TYPE"]
+        logger.info(f"Setup Collection for type {self._TYPE} from {self._HOST}")
+
+        if syslogng:
+            self.persist = Persist(f"mimecast_{self._TYPE}", defaults={"nextPage": ""})
 
         return True
 
     def run(self):
-        """Simple Run method to create the loop"""
-        asyncio.run(self.receive_batch())
+        """Called by Syslog-ng to start the function"""
 
-    def backoff_hdlr(details):
-        logger.warning(
-            "Backing off {wait:0.1f} seconds after {tries} tries "
-            "calling function {target} with args {args} and kwargs "
-            "{kwargs}".format(**details)
+        logger.info("Run called by syslog-ng")
+        asyncio.run(self.run_async())
+
+    def request_exit(self):
+        """Called by syslog NG on exit"""
+
+        logger.info("Exit called by syslog-ng")
+        self.cancelled = True
+
+    async def run_async(self):
+        """Actual start of process"""
+
+        async with httpx.AsyncClient(http2=True) as session:
+            token = None
+            nextPage = None
+            if syslogng and self.persist["nextPage"] != "":
+                nextPage = self.persist["nextPage"]
+
+            thisPage = None
+
+            # Tokens have an expiration point in seconds so we start
+            # The loop with an expired token so we can auth
+            expires = datetime.now() - timedelta(seconds=30)
+            while not self._cancelled:
+                logger.info(f"expires in {expires-datetime.now()}")
+                if datetime.now() >= expires:
+                    token = await self.get_access_token(session)
+                    logger.debug(f"Auth Token {token}")
+                    expires = (
+                        datetime.now()
+                        + timedelta(seconds=int(token["expires_in"]))
+                        + timedelta(seconds=-30)
+                    )
+
+                # Keep up with current page if the nextPage does not change
+                # We don't want to re-ingest the same data
+                thisPage = nextPage
+
+                # mimecast returns a list of batches which are signed urls
+                # pointing to amazon s3 object in form of json.gz files
+                batch_list = await self.get_next_batch(session, token, self._TYPE, nextPage)
+
+                if "@nextPage" in batch_list:
+                    nextPage = batch_list["@nextPage"]
+
+                    if (
+                        "value" in batch_list
+                        and len(batch_list["value"]) > 0
+                        and (nextPage != thisPage or not thisPage)
+                    ):
+                        tasks = []
+                        for link in batch_list["value"]:
+                            tasks.append(self.batch_to_events(session, link))
+                        await asyncio.gather(*tasks)
+                    elif nextPage == thisPage and thisPage:
+                        logger.info("sleeping page did not move")
+                        time.sleep(60)
+
+                    if syslogng:
+                        self.persist["nextPage"] = nextPage
+                else:
+                    logger.warning(f"sleeping no data on page {batch_list}")
+                    time.sleep(60)
+
+    def _get_url(self, path=None, args=None):
+        return furl().set(
+            scheme="https",
+            host=self._HOST,
+            path=path,
+            args=args,
         )
 
     @backoff.on_exception(backoff.expo, httpx.ReadTimeout)
-    async def get_access_token(session):
-        requestUrl = "https://api.services.mimecast.com/oauth/token"
-        payload = f"client_id={MIMECAST_CLIENT_ID}&client_secret={MIMECAST_CLIENT_SECRET}&grant_type=client_credentials"
+    async def get_access_token(self, session):
+        """Get an auth token"""
+        requestUrl = self._get_url(path="oauth/token").url
+        # This payload looks like query string args but this is the format the api requires
+
+        payload = "&".join(
+            [
+                "=".join(["client_id", self._CLIENT_ID]),
+                "=".join(["client_secret", self._CLIENT_SECRET]),
+                "=".join(["grant_type", "client_credentials"]),
+            ]
+        )
+        # Note the accept encoding
         requestHeaders = {
             "Content-Type": "application/x-www-form-urlencoded",
             "Accept": "application/json",
-            # "Accept-Encoding": "gzip",
+            "Accept-Encoding": "br;q=1.0, gzip;q=0.8, deflate;q=0.1",
         }
-
+        logger.debug(f"Auth Attempt {requestUrl} {requestHeaders}")
         response = await session.post(requestUrl, headers=requestHeaders, data=payload)
         response.raise_for_status()
-        print(response.json())
+        logger.debug(f"Auth Response {response.json()}")
         return response.json()
 
-    @backoff.on_exception(backoff.expo, httpx.ReadTimeout, max_time=60, on_backoff=backoff_hdlr)
-    async def get_next_batch(session, access_token, type, nextPage):
+    @backoff.on_exception(backoff.expo, httpx.ReadTimeout, max_time=60, on_backoff=_backoff_handler)
+    async def get_next_batch(self, session, access_token, feed_type, nextPage):
+        """Use the auth token to get a batch of urls to process"""
+        request_furl = self._get_url(
+            path="v1/siem/batch/events/cg",
+            args={
+                "pageSize": "100",
+                "type": feed_type,
+            },
+        )
+
         if nextPage:
-            requestUrl = f"https://api.services.mimecast.com/v1/siem/batch/events/cg?type={type}&pageSize=100&nextLink={nextPage}"
-        else:
-            requestUrl = f"https://api.services.mimecast.com/v1/siem/batch/events/cg?type={type}&pageSize=100"
+            request_furl.args["nextLink"] = nextPage
 
         requestHeaders = {
             "Authorization": f"Bearer {access_token['access_token']}",
             "Accept": "application/json",
-            "Accept-Encoding": "gzip",
+            "Accept-Encoding": "br;q=1.0, gzip;q=0.8, deflate;q=0.1",
         }
-        response = await session.get(requestUrl, headers=requestHeaders)
+        response = await session.get(request_furl.url, headers=requestHeaders)
         response.raise_for_status()
 
+        # logger.debug(response.json())
         return response.json()
 
-    async def batch_to_events(session, link):
+    @backoff.on_exception(backoff.expo, httpx.ReadTimeout, max_time=60, on_backoff=_backoff_handler)
+    async def batch_to_events(self, session, link):
         chunk_size: int = 8192
         url = link["url"]
+        f_url = furl(url).set(args=[])
+        head, filename = os.path.split(str(f_url.path))
+        logger.debug(f"collecting {filename} from {f_url.url}")
 
         # response = await session.get(url)
         with tempfile.TemporaryDirectory() as tmpdirname:
@@ -91,98 +243,27 @@ class LogSource(LogSource):
                 for line in j:
                     count += 1
                     data = loads(line)
-                    # print(data) #TODO: SEND DATA
-                # print(f"found {count}")
+                    CleanEvent(data)
+                    event_time = datetime.utcfromtimestamp(int(data["timestamp"]) / 1000).replace(
+                        tzinfo=pytz.utc
+                    )
+                    logger.debug(
+                        f"source={filename} line={count} parsedtime_stamp={event_time} data={data}"
+                    )
 
-    async def bulk(session, batch):
-        for link in batch["value"]:
-            tasks = []
-            tasks.append(batch_to_events(session, link))
-            await asyncio.gather(*tasks)
+                    if syslogng:
+                        message = orjson.dumps(data)
+                        record_lmsg = LogMessage(message)
+                        record_lmsg.set_timestamp(event_time)
+                        self.post_message(record_lmsg)
+            logger.info(f"Completed {count} from {filename}")
 
-    async def receive_batch(self):
-        params = {
-            "limit": MAX_CHUNK_SIZE,
-            "stream_type": EnterpriseEventsStreamType.ADMIN_LOGS,
-            "stream_position": self.persist["stream_position"],
-        }
-        timeout = 5
-        while not self.cancelled:
-            # self.cancelled = True
-            # try:
-            box_response = self._get_events(params)
-            entries = box_response["entries"]
-            for entry in entries:
-                event = EventStream.clean_event(entry)
-                record_lmsg = LogMessage(orjson.dumps(event))
-                self.post_message(record_lmsg)
 
-            self.persist["stream_position"] = box_response["next_stream_position"]
-            params["stream_position"] = box_response["next_stream_position"]
-            logger.info(
-                f"Posted count={len(entries)} next_stream_position={params['stream_position']}"
-            )
+def main():
+    source = LogSourcePlugin()
+    source.init({})
+    source.run()
 
-    def backoff_hdlr_exp(details):
-        logger.info(
-            "Backing off {wait:0.1f} seconds after {tries} tries with args {args}".format(**details)
-        )
 
-    def backoff_hdlr_pred(details):
-        logger.info(
-            "Backing off {wait:0.1f} seconds after {tries} tries "
-            "with args {args} result {value}".format(**details)
-        )
-
-    @backoff.on_exception(
-        backoff.expo,
-        (requests.exceptions.Timeout, requests.exceptions.ConnectionError),
-        max_time=300,
-        on_backoff=backoff_hdlr_exp,
-    )
-    @backoff.on_predicate(
-        backoff.expo,
-        lambda x: x["entries"] == [],
-        max_time=300,
-        on_backoff=backoff_hdlr_pred,
-    )
-    def _get_events(self, params):
-        box_response = self._client.make_request(
-            "GET", self._client.get_url("events"), params=params, timeout=30
-        )
-        result = box_response.json()
-        return result
-
-    @staticmethod
-    def clean_event(source_dict: dict):
-        """
-        Delete keys with the value ``None``  or ```` (empty) string in a dictionary, recursively.
-        Remove empty list and dict objects
-
-        This alters the input so you may wish to ``copy`` the dict first.
-        """
-        # For Python 3, write `list(d.items())`; `d.items()` won’t work
-        # For Python 2, write `d.items()`; `d.iteritems()` won’t work
-        for key, value in list(source_dict.items()):
-            if value is None:
-                del source_dict[key]
-            elif isinstance(value, str) and value in ("", "None", "none"):
-                del source_dict[key]
-            elif isinstance(value, str):
-                if value.endswith("\n"):
-                    value = value.strip("\n")
-
-                if value.startswith('{"'):
-                    try:
-                        value = orjson.loads(value)
-                        EventStream.clean_event(value)
-                        source_dict[key] = value
-                    except orjson.JSONDecodeError:
-                        pass
-            elif isinstance(value, dict) and not value:
-                del source_dict[key]
-            elif isinstance(value, dict):
-                EventStream.clean_event(value)
-            elif isinstance(value, list) and not value:
-                del source_dict[key]
-        return source_dict  # For convenience
+if __name__ == "__main__":
+    main()
